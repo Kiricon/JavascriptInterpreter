98a5b0a7693ce236aca19f039237f67f2df0a841
Kiricon <kiricon@live.com>
2017-01-17 14:34:54 -0700

    Added comments to parse

--- parser/parser.js
+++ parser/parser.js
@@ -2,6 +2,10 @@ const Lexer = require('./lexer/lexer');
 const Token = require('./../token/token').Token;
 const Types = require('./../token/token').Types;
 
+/**
+ * This is where we parse our tokens in to our 
+ * abstract syntax tree
+ */
 class Parser {
     constructor(string) {
         this.lexer = new Lexer(string);
@@ -14,12 +18,18 @@ class Parser {
         this.parse();
     }
 
+    /**
+     * Itterate to the next token 
+     */
     nextToken() {
         this.curToken = this.peekToken;
         this.peekToken = this.lexer.tokens[i];
         i++;
     }
 
+    /**
+     * Loop through all tokens and parse out statements
+     */
     parse() {
         while(this.curToken.type != Types.EOF){
             let statement = this.parseStatement();
