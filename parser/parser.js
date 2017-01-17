9780dd7795c5f6adad2bc436adb4f34a3e45f7f6
Kiricon <kiricon@live.com>
2017-01-17 13:46:40 -0700

    Built our parse loop for parseing out statements.

--- parser/parser.js
+++ parser/parser.js
@@ -8,14 +8,26 @@ class Parser {
         this.curToken;
         this.peekToken;
         this.i = 0;
+        this.statements = [];
         this.nextToken();
         this.nextToken();
+        this.parse();
     }
 
     nextToken() {
         this.curToken = this.peekToken;
         this.peekToken = this.lexer.tokens[i];
         i++;
+    }
+
+    parse() {
+        while(this.curToken.type != Types.EOF){
+            let statement = this.parseStatement();
+            if(statement != null) {
+                this.statements.push(statement);
+            }
+            this.nextToken();
+        }
     }
 
 }
